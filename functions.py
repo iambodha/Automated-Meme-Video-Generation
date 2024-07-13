3048ce7d6628bd6d2078ccbf065d7972249833fd
iambodha <165776420+iambodha@users.noreply.github.com>
2024-07-13 11:17:09 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,8 +1,17 @@
 import os,csv,shutil,subprocess,pandas as pd,hashlib,re
 from tqdm import tqdm
 import time,random
+from PIL import Image
+import cv2,pytesseract
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import requests
 from moviepy.editor import *
+from PIL import Image
+from pydub import AudioSegment
+import imageio
+from watermark import File, Watermark, apply_watermark, Position
 import uuid
 from variables import *
 
@@ -126,3 +135,86 @@ def getFirstFrame(makingStage2,makingStage3):
   if video_file.endswith(".mp4")or video_file.endswith(".avi")or video_file.endswith(".mkv"):
    video_path=os.path.join(makingStage2,video_file)
    cap=cv2.VideoCapture(video_path)
+
+def getAllCoveredFrames(makingStage3,makingStage4):
+ if not os.path.exists(makingStage4):
+  os.makedirs(makingStage4)
+ for filename in os.listdir(makingStage3):
+  if filename.endswith(".jpg"):
+   img_path=os.path.join(makingStage3,filename)
+   img=cv2.imread(img_path)
+   img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+   print(img.shape)
+   hImg,wImg,_=img.shape
+   boxes=pytesseract.image_to_data(img)
+   for a,b in reversed(list(enumerate(boxes.splitlines()))):
+    if a!=0:
+     b=b.split()
+     print(b)
+     if len(b)==12 and float(b[10])>=0.90:
+      x,y,w,h=map(int,b[6:10])
+      new_w=int(w*1.15)
+      new_h=int(h*1.15)
+      new_x=max(0,x-int((new_w-w)/2))
+      new_y=max(0,y-int((new_h-h)/2))
+      img[new_y:new_y+new_h,new_x:new_x+new_w]=[255,255,255]
+      img=cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
+      output_path=os.path.join(makingStage4,f'{a}_{filename}')
+      cv2.imwrite(output_path,img)
+      img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+def getTextList(makingStage3):
+ if not makingStage3.endswith('/'):
+  makingStage3+='/'
+ textDict={}
+ files=[f for f in os.listdir(makingStage3)if f.lower().endswith('.jpg')]
+ for file in files:
+  file_path=makingStage3+file
+  img=cv2.imread(file_path)
+  img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+  textBox=pytesseract.image_to_data(img)
+  text=""
+  for a,b in enumerate(textBox.splitlines()):
+   if a!=0:
+    b=b.split()
+    if len(b)==12 and float(b[10])>=0.90:
+     text+=b[11]+" "
+  text=re.sub(r'\|','I',text)
+  image_title=os.path.splitext(file)[0]
+  textDict[image_title]=text
+ print(textDict)
+ return textDict
+
+def getAudioFiles(textDict,makingStage5):
+ chrome_options=Options()
+ chrome_options.add_argument("--headless")
+ driver=webdriver.Chrome(executable_path=webdriver_path,options=chrome_options)
+ url='https://beta.meetaugie.com/sign-in'
+ driver.get(url)
+ email_input=driver.find_element(By.ID,'email')
+ email_input.send_keys('your@gmail.com')
+ password_input=driver.find_element(By.ID,'password')
+ password_input.send_keys('Your')
+ login_button=driver.find_element(By.XPATH,"//button[contains(.,'Login')]")
+ login_button.click()
+ time.sleep(2)
+ create_url="https://beta.meetaugie.com/create"
+ driver.get(create_url)
+ time.sleep(2)
+ script_button=driver.find_element(By.XPATH,"//button[contains(.,'I have a script that I want to turn into a video')]")
+ script_button.click()
+ go_button=driver.find_element(By.XPATH,"//button[contains(.,"LET'S GO")]")
+ go_button.click()
+ editing_button=driver.find_element(By.XPATH,"//button[@aria-label='Enable text editing']")
+ editing_button.click()
+ textarea=driver.find_element(By.ID,'scriptEditorTextArea')
+ textarea.clear()
+ textarea.send_keys('Test')
+ done_button=driver.find_element(By.XPATH,"//button[@class='bg-[#442C6B] w-[94px] h-6 text-tActive text-[15px] py-2 px-4 rounded-full inline-flex justify-center items-center w-48']")
+ done_button.click()
+ voice_button=driver.find_element(By.XPATH,"//button[@aria-label='Choose a Voice']")
+ voice_button.click()
+ time.sleep(15)
+ option_voice=driver.find_element(By.XPATH,"//div[text()='Jeremy']")
+ option_voice.click()
+ for image_title,text in textDict.items():
